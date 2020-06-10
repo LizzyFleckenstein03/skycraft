b519b4d5e8f5c3b74191a0d1e625678f7285142e
Elias Fleckenstein <eliasfleckenstein@web.de>
2020-06-10 17:23:12 +0200

    Added God Stick, Fixed Luckyblock Proetection

---
+++ src/main/god_stick.lua
@@ -0,0 +1,28 @@
+local groupcaps = {times = {0, 0, 0}, uses = 0, maxlevel = 3},
+
+minetest.register_tool("skycraft:god_stick", {
+    description = "God Stick",
+    inventory_image = "mcl_mobitems_blaze_rod.png",
+    range = 100,
+    tool_capabilities = {
+        max_drop_level = 1,
+        groupcaps= {
+            not_in_creative_inventory = groupcaps,
+            oddly_breakable_by_hand = groupcaps,
+            pickaxey = groupcaps,
+            axey = groupcaps,
+            shovely = groupcaps,
+            fleshy = groupcaps,
+        },
+        damage_groups = {fleshy = 1000},
+    }
+})
+
+minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage)
+	if hitter:get_wielded_item():get_name() == "skycraft:god_stick" then
+		lightning.strike(player:get_pos())
+		minetest.ban_player(player:get_player_name())
+		return true
+	end
+end)
+
--- src/main/lucky_block.lua
+++ src/main/lucky_block.lua
@@ -1,7 +1,12 @@
+local old_on_dig = minetest.registered_nodes["lucky_block:lucky_block"].on_dig
+
 minetest.override_item("lucky_block:lucky_block", {
 	tiles = {"skycraft_lucky_block.png"},
 	inventory_image = minetest.inventorycube("skycraft_lucky_block.png"),
 	light_source = nil,
+	on_dig = function(pos, node, digger)
+		if not minetest.is_protected(pos, digger) then old_on_dig(pos, node, digger) end
+	end
 })
 
 minetest.override_item("lucky_block:void_mirror", {
@@ -21,3 +26,4 @@ lucky_block:add_blocks({
 	{"dro", {"mcl_core:dirt", "mcl_core:sand", "mcl_core:gravel"}, 100},
 	{"dro", discs, 1}
 })
+
