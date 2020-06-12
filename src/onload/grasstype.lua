04a712757f8d2d4992a56f42f2a36f96cf99c3bb
Elias Fleckenstein <eliasfleckenstein@web.de>
2020-06-12 09:33:48 +0200

    Changed Grass Block Type Everywhere

---
+++ src/onload/grasstype.lua
@@ -0,0 +1,3 @@
+function mcl_core.get_grass_block_type(pos)
+	return {name = "mcl_core:dirt_with_grass", param2=0}
+end
--- src/onload/lobby_grass.lua
+++
@@ -1,7 +0,0 @@
-local old_get_grass_block_type = mcl_core.get_grass_block_type
-function mcl_core.get_grass_block_type(pos)
-	if pos.y > 5000 then
-		return {name = "mcl_core:dirt_with_grass", param2=0}
-	end
-	return old_get_grass_block_type(pos)
-end
